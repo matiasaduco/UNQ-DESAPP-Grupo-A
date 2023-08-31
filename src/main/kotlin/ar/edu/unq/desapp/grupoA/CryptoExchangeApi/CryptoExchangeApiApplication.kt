7286a7462bf23d40d4dc692361a7bf9d59c6f714
Lucas Alvarez <alvarezlucas2787@gmail.com>
2023-08-31 19:31:01 -0300

    first commit

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/CryptoExchangeApiApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.desapp.grupoA.CryptoExchangeApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoExchangeApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoExchangeApiApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/CryptoExchangeApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoA.CryptoExchangeApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoExchangeApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
