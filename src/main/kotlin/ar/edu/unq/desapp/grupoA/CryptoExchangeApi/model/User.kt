5a05afa9319b53f09ad407d20eb6096b2605999a
Matias Aduco <matias.aduco@gmail.com>
2023-09-06 19:42:13 -0300

    Agregué el modelo de User. Implementé Lombok para poder utilizar sus annotations.

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/model/User.kt
@@ -0,0 +1,14 @@
+package ar.edu.unq.desapp.grupoA.CryptoExchangeApi.model
+
+class User(
+    var name: String,
+    var surname: String,
+    var email: String,
+    var address: String,
+    var password: String,
+    var cvu: String,
+    var walletAddress: String,
+) {
+    var operations: Int = 0
+    var reputation: Float = 0f
+}
