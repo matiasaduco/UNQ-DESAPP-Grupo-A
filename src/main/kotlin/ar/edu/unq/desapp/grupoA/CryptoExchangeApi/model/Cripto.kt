1b048dd55ee748feae765bddced79fb8119b3b05
Lucas Alvarez <alvarezlucas2787@gmail.com>
2023-09-06 12:13:43 -0300

    Cripto model and testing

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/model/Cripto.kt
@@ -0,0 +1,9 @@
+package ar.edu.unq.desapp.grupoA.CryptoExchangeApi.model
+
+import java.time.LocalDate
+import java.util.Date
+
+class Cripto( val name:String, var  price:Float, val time: LocalDate ){
+
+
+}
