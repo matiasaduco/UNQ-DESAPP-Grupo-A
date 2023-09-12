35634df094413edea0e94476c67bd5888e831edd
Lucas Alvarez <alvarezlucas2787@gmail.com>
2023-09-12 17:51:11 -0300

    Add swagger 3 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	//implementation("org.springframework.boot:spring-boot-starter-security")
+	// https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
