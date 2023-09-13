49708e6527ddfd8efc52a174a3cc2ef342376689
Matias Aduco <matias.aduco@gmail.com>
2023-09-13 19:27:48 -0300

    Cambié el tipo de dato en User para que el CVU y Wallet Address para que solo puedan ser números. Ahora el controller se comunica con el Service para guardar al usuario. el getUser requiere un userId, aunque creo que no hace falta un getUser en este dominio.

--- src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/services/impl/UserServiceImpl.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/services/impl/UserServiceImpl.kt
@@ -2,10 +2,21 @@ package ar.edu.unq.desapp.grupoA.CryptoExchangeApi.services.impl
 
 import ar.edu.unq.desapp.grupoA.CryptoExchangeApi.model.User
 import ar.edu.unq.desapp.grupoA.CryptoExchangeApi.services.UserService
+import org.springframework.stereotype.Service
 
+@Service
 class UserServiceImpl: UserService {
-    override fun signin(): User {
-        TODO("Not yet implemented")
+    // Eliminar en cuanto tengamos implementación de H2
+    var userRepository : MutableList<User> = mutableListOf()
+
+    override fun signin(user: User): User {
+        try {
+            userRepository.add(user)
+            return user
+        }
+        catch (exception: Exception) {
+            throw Exception("Error al ingresar el usuario $user")
+        }
     }
 
     override fun login(): String {
