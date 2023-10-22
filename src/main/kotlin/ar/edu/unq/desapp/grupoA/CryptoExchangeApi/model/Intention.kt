71811f5bffb29a8f2407b5949249dbf1309e60b5
Matias Aduco <matias.aduco@gmail.com>
2023-10-22 12:29:01 -0300

    Refactor general de todo lo que corresponde a Transacciones.

--- src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/model/Intention.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoA/CryptoExchangeApi/model/Intention.kt
@@ -13,19 +13,20 @@ class Intention(
     @ManyToOne(fetch = FetchType.EAGER)
     val user: User
 ) {
+
     fun canBeConfirmed(): Boolean {
-        if (operation == IntentionType.BUY) {
-            return crypto.price <= intentionCryptoPrice
+        return if (operation == IntentionType.BUY) {
+            crypto.price <= intentionCryptoPrice
         } else {
-            return crypto.price >= intentionCryptoPrice
+            crypto.price >= intentionCryptoPrice
         }
     }
 
     fun addressToSend(): String {
-        if (operation == IntentionType.BUY) {
-            return user.walletAddress.toString()
+        return if (operation == IntentionType.BUY) {
+            user.walletAddress.toString()
         } else {
-            return user.cvu.toString()
+            user.cvu.toString()
         }
     }
 
